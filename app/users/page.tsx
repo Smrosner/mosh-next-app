da233556b27d1781a891dc378d6133e6da77caea
Shay Rosner <shaymrosner@gmail.com>
2023-11-29 21:02:32 -1000

    static dynamic rendering - ts35.49

--- app/users/page.tsx
+++ app/users/page.tsx
@@ -8,19 +8,22 @@ interface User {
 const UsersPage = async () => {
   const res = await fetch(
     "https://jsonplaceholder.typicode.com/users",
-    // { cache: 'no-store' } - usefull if you have data that changes frequently
-    { next: { revalidate: 10 } } // this 'next' obj can specify config params that nextjs specific
+    { cache: 'no-store' } // - usefull if you have data that changes frequently
+    // { next: { revalidate: 10 } } // this 'next' obj can specify config params that nextjs specific
     // revalidate: 10 - means that next js is going to run a background job and get fresh data from the backend every 10s
   );
   const users: User[] = await res.json();
   return (
     <>
       <h1>Users</h1>
+      <p>{new Date().toLocaleTimeString()}</p>
       <ul>
-        {users.map(user => <li key={user.id}>{user.name}</li>)}
+        {users.map((user) => (
+          <li key={user.id}>{user.name}</li>
+        ))}
       </ul>
     </>
-  )
+  );
 }
 
 export default UsersPage;
