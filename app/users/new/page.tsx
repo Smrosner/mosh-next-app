f08597d60bfd83813237199ab9c8c6ec9aa09856
Your Name <you@example.com>
2023-12-01 18:07:20 -1000

    add nav to users page with buttons

--- app/users/new/page.tsx
+++ app/users/new/page.tsx
@@ -1,7 +1,16 @@
+'use client'
+// import { useRouter } from 'next/router'; this is the default import location and needs to be chanced to next/navigation
+import { useRouter } from "next/navigation";
 import React from 'react'
 
 const NewUserPage = () => {
-  return <div>NewUserPage</div>;
+  const router = useRouter();
+  return (
+    <button
+      className='btn btn-primary'
+      onClick={() => router.push('/users')}
+    >Create</button>
+    )
 };
 
 export default NewUserPage;
