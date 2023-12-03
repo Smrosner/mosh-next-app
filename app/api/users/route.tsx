0c8cbb0b3a3e5f06c78755443b4dcae5497e50db
Your Name <you@example.com>
2023-12-02 23:40:13 -1000

    fix import

--- app/api/users/route.tsx
+++ app/api/users/route.tsx
@@ -1,5 +1,5 @@
 import { NextRequest, NextResponse } from "next/server";
-import schema from "../schema";
+import schema from "./schema";
 
 export function GET(request: NextRequest) {
   return NextResponse.json([
@@ -16,6 +16,6 @@ export async function POST(request: NextRequest) {
   // postman post req http://localhost:3000/api/users
   if (!validation.success)
     return NextResponse.json(validation.error.errors), { status: 400 };
-// Else, return 201
-return NextResponse.json({ id: 1, name: body.name }, { status: 201 });
+  // Else, return 201
+  return NextResponse.json({ id: 1, name: body.name }, { status: 201 });
 }
