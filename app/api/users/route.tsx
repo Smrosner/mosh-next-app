c928890be5f353cd2b5d27d6c3de934981b3254d
Your Name <you@example.com>
2023-12-02 23:01:53 -1000

    adds user post req

--- app/api/users/route.tsx
+++ app/api/users/route.tsx
@@ -2,7 +2,15 @@ import { NextRequest, NextResponse } from "next/server";
 
 export function GET(request: NextRequest) {
   return NextResponse.json([
-    { id: 1, name: 'Shay'},
-    { id: 2, name: 'Brian' },
-  ])
+    { id: 1, name: "Shay" },
+    { id: 2, name: "Brian" },
+  ]);
+}
+
+export async function POST(request: NextRequest) {
+  const body = await request.json();
+  // postman post req http://localhost:3000/api/users
+  if (!body.name)
+    return NextResponse.json({ error: "Name is required" }, { status: 400 });
+  return NextResponse.json({ id: 1, name: body.name }, { status: 201 });
 }
