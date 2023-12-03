758b4ad9c4e3a0e8c12c43ddfd6d68ddb1f31c41
Your Name <you@example.com>
2023-12-02 23:28:40 -1000

    delete user request, with comments for all

--- app/api/users/[id]/route.tsx
+++ app/api/users/[id]/route.tsx
@@ -10,9 +10,11 @@ export function GET(
   request: NextRequest,
   { params }: { params: { id: number } }
 ) {
+  // Fetch data from db
+  // If not found, return 404 error
   if (params.id > 10)
     return NextResponse.json({ error: "User not found" }, { status: 404 });
-
+// Else return data
   return NextResponse.json({ id: 1, name: "Shay" });
 }
 
@@ -21,9 +23,28 @@ export async function PUT(
   { params }: { params: { id: number } }
 ) {
   const body = await request.json();
+  // Validate the request body
   if (!body.name)
-    return NextResponse.json({ error: "Name is required" }, { status: 400 });
-  if (params.id > 10)
-    return NextResponse.json({ error: "User not found" }, { status: 404 });
+  // If invalid, return 400
+return NextResponse.json({ error: "Name is required" }, { status: 400 });
+// Fetch the user with the given id
+if (params.id > 10)
+// If doesn't exist, return 404
+return NextResponse.json({ error: "User not found" }, { status: 404 });
+// Update the user
+// Return the updated user
   return NextResponse.json({ id: 1, name: body.name });
+}
+
+export function DELETE(
+  request: NextRequest,
+  { params }: { params: { id: number } }
+) {
+  // Fetch user from db
+  // If not found, return 404
+  if (params.id > 10)
+    return NextResponse.json({ error: 'User not found' }, { status: 404 })
+  // Delete user
+  // Return 200
+  return NextResponse.json({})
 }
--- app/api/users/route.tsx
+++ app/api/users/route.tsx
@@ -9,8 +9,11 @@ export function GET(request: NextRequest) {
 
 export async function POST(request: NextRequest) {
   const body = await request.json();
+  // Validate
+  // If invalid, return 400
   // postman post req http://localhost:3000/api/users
   if (!body.name)
-    return NextResponse.json({ error: "Name is required" }, { status: 400 });
-  return NextResponse.json({ id: 1, name: body.name }, { status: 201 });
+  return NextResponse.json({ error: "Name is required" }, { status: 400 });
+// Else, return 201
+return NextResponse.json({ id: 1, name: body.name }, { status: 201 });
 }
