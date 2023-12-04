aa352cfc353aaaafaf05348401fabe0914daedc5
Your Name <you@example.com>
2023-12-03 21:22:16 -1000

    gets user data from db

--- app/api/users/[id]/route.tsx
+++ app/api/users/[id]/route.tsx
@@ -1,5 +1,6 @@
 import { NextRequest, NextResponse } from "next/server";
 import schema from "../schema";
+import { prisma } from "@/prisma/client";
 
 interface Props {
   params: {
@@ -7,16 +8,19 @@ interface Props {
   };
 }
 
-export function GET(
+export async function GET(
   request: NextRequest,
-  { params }: { params: { id: number } }
+  { params }: { params: { id: string } }
 ) {
   // Fetch data from db
+  const user = await prisma.user.findUnique({
+    where: { id: parseInt(params.id) },
+  });
   // If not found, return 404 error
-  if (params.id > 10)
+  if (!user)
     return NextResponse.json({ error: "User not found" }, { status: 404 });
-// Else return data
-  return NextResponse.json({ id: 1, name: "Shay" });
+  // Else return data
+  return NextResponse.json(user);
 }
 
 export async function PUT(
@@ -27,14 +31,14 @@ export async function PUT(
   // Validate the request body
   const validation = schema.safeParse(body);
   if (!validation.success)
-  // If invalid, return 400
-return NextResponse.json(validation.error.errors, { status: 400 });
-// Fetch the user with the given id
-if (params.id > 10)
-// If doesn't exist, return 404
-return NextResponse.json({ error: "User not found" }, { status: 404 });
-// Update the user
-// Return the updated user
+    // If invalid, return 400
+    return NextResponse.json(validation.error.errors, { status: 400 });
+  // Fetch the user with the given id
+  if (params.id > 10)
+    // If doesn't exist, return 404
+    return NextResponse.json({ error: "User not found" }, { status: 404 });
+  // Update the user
+  // Return the updated user
   return NextResponse.json({ id: 1, name: body.name });
 }
 
@@ -45,8 +49,8 @@ export function DELETE(
   // Fetch user from db
   // If not found, return 404
   if (params.id > 10)
-    return NextResponse.json({ error: 'User not found' }, { status: 404 })
+    return NextResponse.json({ error: "User not found" }, { status: 404 });
   // Delete user
   // Return 200
-  return NextResponse.json({})
+  return NextResponse.json({});
 }
--- prisma/client.ts
+++ prisma/client.ts
@@ -8,9 +8,7 @@ declare global {
   var prisma: undefined | ReturnType<typeof prismaClientSingleton>;
 }
 
-const prisma = globalThis.prisma ?? prismaClientSingleton();
-
-export default prisma;
+export const prisma = globalThis.prisma ?? prismaClientSingleton();
 
 if (process.env.NODE_ENV !== "production") globalThis.prisma = prisma;
 
